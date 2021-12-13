98c877f599d8ad4493321344f3542a56ac5f3edd
Devishree Venkatesh <shree2410@gmail.com>
2021-12-12 19:09:17 -0500

    created volunteer crudviews and Registerform.

---
+++ ExperienceIT.Web/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExperienceIT.Web.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        //public string PhoneNumber { get; set; }removed phone number because it's already exist.
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+
+
+    }
+}
