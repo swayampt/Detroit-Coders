37bd88237de19597abc2ccf4e3d34a592e1fc3c3
Devishree Venkatesh <shree2410@gmail.com>
2021-12-21 19:56:39 -0500

    Changes for SendEmail in sign up,workplace dropdown fix and volunteers list page with view model when admin logged in

---
+++ ExperienceIT.Web/ViewModels/VolunteerOrganizationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ExperienceIT.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExperienceIT.Web.ViewModels
+{
+    public class VolunteerOrganizationViewModel
+    {
+        public IEnumerable<VolunteerMaster> volunteers { get; set; }
+        public List<OrganizerMaster> organizationList { get; set; }
+    }
+}
